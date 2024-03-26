495f1b595754cca842832ae81f8c858d874c900c
Rusiek <szymon.rusiecki@proton.me>
2024-03-26 18:03:12 +0100

    fix imports

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import sys
 from algorithms.example import Example
 from benchmark.evaluate import Evaluate
-from loader.loader import load_graph, load_constraints, load_metro_params
+from loader import load_graph, load_constraints, load_metro_params
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    G = load_graph(path)
-    metro = load_metro_params(path)
-    max_cost = load_constraints(path)
+    dataset = sys.argv[1]
+    G = load_graph(dataset)
+    metro = load_metro_params(dataset)
+    max_cost = load_constraints(dataset)
     worker = Evaluate(G, metro, max_cost)
     solution = Example(G).find_solution()
     output = worker.evaluate(solution)
